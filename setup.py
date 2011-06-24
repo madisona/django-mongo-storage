dc4a67c0f8dd4a2e6f779965e564e6019c931758
Aaron Madison <aaron.madison@theimtgroup.com>
2011-06-24 11:33:06 -0500

    added license

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ CLASSIFIERS = (
     "Environment :: Web Environment",
     "Framework :: Django",
     "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Topic :: Software Development",
